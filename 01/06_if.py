d18813a2edd4f2c9612c2a55c5595da1c9946fa8
Alexey Vasyukov <vasyukov@gmail.com>
2020-09-01 18:17:01 +0300

    Seminar 01: Hello, Python

---
+++ 01/06_if.py
@@ -0,0 +1,12 @@
+"""
+Без if-ов жить, конечно, нельзя. В Python-е они выглядят примерно так.
+"""
+
+a = int(input())
+
+if a > 0:
+    print("Positive")
+elif a < 0:
+    print("Negative")
+else:
+    print("Zero")
